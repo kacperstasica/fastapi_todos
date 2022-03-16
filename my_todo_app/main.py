92ddc808d35e3ae53eaec2374973beb125815920
Kacper Stasica <kacperstasica@macbook-pro-kacper-s.home>
2022-03-16 13:00:04 +0100

    Code cleanup

--- my_todo_app/main.py
+++ my_todo_app/main.py
@@ -1,10 +1,9 @@
 import uvicorn
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Depends
 
 import models
 from database import engine
 from routers import auth, todos
-from company import companyapis
 
 
 app = FastAPI()
@@ -13,13 +12,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
-# external routing - we add prefixes, tags and responses unique to our app, not to the APIs
-app.include_router(
-    companyapis.router,
-    prefix="/companyapis",
-    tags=["companyapis"],
-    responses={status.HTTP_418_IM_A_TEAPOT: {"description": "Internal Use Only"}}
-)
 
 
 if __name__ == '__main__':
